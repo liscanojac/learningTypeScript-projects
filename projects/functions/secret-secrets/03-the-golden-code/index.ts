7b1254df03d3007a405c0eb7179bc7a3b9dd046e
Juan Canelon <liscanojac@gmail.com>
2022-07-25 17:09:06 -0300

    feat: theGoldenCode chapter5

--- projects/functions/secret-secrets/03-the-golden-code/index.ts
+++ projects/functions/secret-secrets/03-the-golden-code/index.ts
@@ -1,2 +1,30 @@
 // Write your createAdvancedCipher function here! ✨
 // You'll need to export it so the tests can run it.
+
+type MakeGuessFunction = (text: string, attempt: number) => string;
+type ValidateGuessFunction = (guess: string) => boolean;
+type CreateAdvancedCipherFunction = (text: string) => string | undefined;
+
+type CodeCrackerParam = {
+	attempts: number;
+	makeGuess: MakeGuessFunction;
+	validateGuess: ValidateGuessFunction;
+};
+
+function createCodeCracker({
+	attempts,
+	makeGuess,
+	validateGuess,
+}: CodeCrackerParam): CreateAdvancedCipherFunction {
+	const createAdvancedCipher: CreateAdvancedCipherFunction = (text) => {
+		for (let i = 0; i < attempts; i++) {
+			let guess = makeGuess(text, i);
+
+			if (validateGuess(guess)) return guess;
+		}
+		return undefined;
+	};
+	return createAdvancedCipher;
+}
+
+export { createCodeCracker };

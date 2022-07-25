ed9bfb966259f1c3e004a263e65a1c8d02875277
Juan Canelon <liscanojac@gmail.com>
2022-07-25 17:04:51 -0300

    feat: incoming cipher Chapter5

--- projects/functions/secret-secrets/01-incoming-cipher/index.ts
+++ projects/functions/secret-secrets/01-incoming-cipher/index.ts
@@ -1,2 +1,29 @@
 // Write your createCipher function here! ✨
 // You'll need to export it so the tests can run it.
+type CipherFunction = (character: string) => string;
+
+function createCipher(cipher: CipherFunction) {
+	function loopThroughMessage(message: string) {
+		let cipheredMessage = "";
+
+		for (let i = 0; i < message.length; i++) {
+			cipheredMessage += cipher(message[i]);
+		}
+		return cipheredMessage;
+	}
+	return loopThroughMessage;
+}
+
+// function createCipher(cipher: CipherFunction) {
+
+//   function loopThroughMessage(message: string) {
+
+//     const map = Array.prototype.map;
+//     const cipheredMessage = map.call(message, (character: string) => cipher(character))
+
+//     return cipheredMessage.join('');
+//   }
+//   return loopThroughMessage;
+// }
+
+export { createCipher };

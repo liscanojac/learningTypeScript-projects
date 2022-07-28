00e5e1166ca0f5c64c7b7061516a94b2c2b29c5c
Juan Canelon <liscanojac@gmail.com>
2022-07-28 13:35:38 -0300

    feat: chap6-arrays deep-equality

--- projects/arrays/analyzing-dna/03-deep-equality/index.ts
+++ projects/arrays/analyzing-dna/03-deep-equality/index.ts
@@ -1,2 +1,9 @@
 // Write your shallowDifferences function here! ✨
 // You'll need to export it so the tests can run it.
+
+export function deepEquality(arrayOne: string[], arrayTwo: string[]): boolean {
+	if (arrayOne.length === arrayTwo.length) {
+		return JSON.stringify(arrayOne) === JSON.stringify(arrayTwo);
+	}
+	return false;
+}

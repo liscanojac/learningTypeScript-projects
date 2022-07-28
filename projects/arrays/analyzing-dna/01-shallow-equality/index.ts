2c7314af20739f6b954a9a525fd026bef64c9dc2
Juan Canelon <liscanojac@gmail.com>
2022-07-28 12:42:14 -0300

    feat: chap6-arrays shallow-equality

--- projects/arrays/analyzing-dna/01-shallow-equality/index.ts
+++ projects/arrays/analyzing-dna/01-shallow-equality/index.ts
@@ -1,2 +1,19 @@
 // Write your shallowEquality function here! ✨
 // You'll need to export it so the tests can run it.
+
+type StringsArray = string[];
+
+export function shallowEquality(
+	arrayOne: StringsArray,
+	arrayTwo: StringsArray
+): boolean {
+	if (arrayOne.length === arrayTwo.length) {
+		for (let i = 0; i < arrayOne.length; i++) {
+			if (arrayOne[i] !== arrayTwo[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+	return false;
+}

cbbe23b4bd63d7669d4f2872204fff666d65ae01
Juan Canelon <liscanojac@gmail.com>
2022-07-28 12:59:10 -0300

    feat: chap6-arrays shallow-differences

--- projects/arrays/analyzing-dna/02-shallow-differences/index.ts
+++ projects/arrays/analyzing-dna/02-shallow-differences/index.ts
@@ -1,2 +1,23 @@
 // Write your shallowDifferences function here! ✨
 // You'll need to export it so the tests can run it.
+
+type ArrayReturned = (string | undefined)[];
+
+export function shallowDifferences(
+	arrayOne: string[],
+	arrayTwo: string[]
+): ArrayReturned | undefined {
+	if (arrayOne.length === arrayTwo.length) {
+		const resultArr: ArrayReturned = [];
+
+		for (let i = 0; i < arrayOne.length; i++) {
+			if (arrayOne[i] === arrayTwo[i]) {
+				resultArr.push(arrayOne[i]);
+				continue;
+			}
+			resultArr.push(undefined);
+		}
+		return resultArr;
+	}
+	return undefined;
+}

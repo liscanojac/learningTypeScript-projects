3100f5592ad0e43b2fed8af7c6b23e4edd77535e
Juan Canelon <liscanojac@gmail.com>
2022-07-28 13:40:49 -0300

    refactor: deep-equaility types arguments

--- projects/arrays/analyzing-dna/03-deep-equality/index.ts
+++ projects/arrays/analyzing-dna/03-deep-equality/index.ts
@@ -1,7 +1,10 @@
 // Write your shallowDifferences function here! ✨
 // You'll need to export it so the tests can run it.
 
-export function deepEquality(arrayOne: string[], arrayTwo: string[]): boolean {
+export function deepEquality(
+	arrayOne: string[][],
+	arrayTwo: string[][]
+): boolean {
 	if (arrayOne.length === arrayTwo.length) {
 		return JSON.stringify(arrayOne) === JSON.stringify(arrayTwo);
 	}

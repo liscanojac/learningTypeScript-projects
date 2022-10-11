87084aa3e01fa3fb1a5cc761d29abc99dc70d018
Juan Canelon <liscanojac@gmail.com>
2022-10-11 15:28:43 -0300

    feat: restaunrants on vacation-planning/interfaces solved

--- projects/interfaces/vacation-planning/02-restaurants/index.ts
+++ projects/interfaces/vacation-planning/02-restaurants/index.ts
@@ -1,2 +1,25 @@
 // Write your groupLandmarks function here! ✨
 // You'll need to export it so the tests can run it.
+
+interface Restaurant {
+	name: string;
+	city: string;
+}
+
+interface RestaurantsGrouped {
+	[i: string]: Array<string>;
+}
+export function groupRestaurants(
+	restaurants: Array<Restaurant>
+): RestaurantsGrouped {
+	const restaurantsGrouped: RestaurantsGrouped = {};
+
+	for (let i = 0; i < restaurants.length; i++) {
+		if (!restaurantsGrouped.hasOwnProperty(restaurants[i].city)) {
+			restaurantsGrouped[restaurants[i].city] = [restaurants[i].name];
+			continue;
+		}
+		restaurantsGrouped[restaurants[i].city].push(restaurants[i].name);
+	}
+	return restaurantsGrouped;
+}

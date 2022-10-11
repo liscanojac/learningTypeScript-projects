7d643d56e7e2153262b840e8cbc427be1b08eb7d
Juan Canelon <liscanojac@gmail.com>
2022-10-11 14:09:34 -0300

    feat: cities vacation-planning/interfaces solved

--- projects/interfaces/vacation-planning/01-cities/index.ts
+++ projects/interfaces/vacation-planning/01-cities/index.ts
@@ -1,2 +1,43 @@
 // Write your describeCity function here! ✨
 // You'll need to export it so the tests can run it.
+
+type Coordinates = [number, number, number];
+
+export interface City {
+	coordinates: {
+		north: Coordinates;
+		west: Coordinates;
+	};
+	name: string;
+	catchphrase?: string;
+}
+
+export function describeCity(city: City): string {
+	const message: Array<string> = [`${city.name}, New York`];
+
+	if (city.catchphrase) {
+		message.push(`* "${city.catchphrase}"`);
+	}
+
+	message.push(
+		`* Located at ${describeCoordinates(
+			city.coordinates.north
+		)}N ${describeCoordinates(city.coordinates.west)}W`
+	);
+
+	return message.join("\n");
+}
+
+function describeCoordinates(coordinates: Coordinates): string {
+	const coordinateUnits = ["°", `'`, `"`];
+	const coordinatesMessage: Array<string> = [];
+	for (let i = 0; i < coordinates.length; i++) {
+		coordinatesMessage.push(
+			`${coordinates[i] < 10 ? `0${coordinates[i]}` : coordinates[i]}${
+				coordinateUnits[i]
+			}`
+		);
+	}
+
+	return coordinatesMessage.join("");
+}

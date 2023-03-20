f105131b9a8bcc869045fbe85aa8e32690f1862a
Juan Canelon <liscanojac@gmail.com>
2023-03-20 15:58:29 -0300

    chore: shape of types started

--- projects/classes/the-shape-of-types/src/index.ts
+++ projects/classes/the-shape-of-types/src/index.ts
@@ -1,2 +1,20 @@
 // Write your classes here! ✨
 // You'll need to export them so the tests can run them.
+
+export abstract class Horror {
+	protected abstract readonly name: string;
+	protected evil: boolean;
+	abstract power: number;
+
+	doBattle(opponent: Horror) {}
+
+	getPower() {}
+
+	protected abstract getPowerFrom(opponent: Horror): number;
+
+	protected abstract isEvil(): boolean;
+}
+
+class Demon extends Horror {
+	protected name: string = "Demon";
+}

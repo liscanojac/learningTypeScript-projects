f32135372c7308c0b7aa3afc630da49133888027
Juan Canelon <liscanojac@gmail.com>
2023-03-20 15:10:49 -0300

    feat: Puppy classes solved

--- projects/classes/classifying-creatures/02-dog-displays/index.ts
+++ projects/classes/classifying-creatures/02-dog-displays/index.ts
@@ -1,2 +1,22 @@
 // Write your Parrot class here! ✨
 // You'll need to export it so the tests can run it.
+export interface PuppyInTheWindow {
+	readonly colors: Array<string>;
+	readonly furriness: number;
+	readonly owner?: string;
+}
+
+export class Puppy implements PuppyInTheWindow {
+	readonly colors: Array<string>;
+	readonly furriness: number;
+	owner: string | undefined;
+
+	constructor(colors: Array<string>, furriness: number) {
+		this.colors = colors;
+		this.furriness = furriness;
+	}
+
+	adopt(newOwner: string) {
+		this.owner = newOwner;
+	}
+}

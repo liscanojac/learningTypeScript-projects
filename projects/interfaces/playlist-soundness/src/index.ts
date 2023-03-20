ec5f0fd50a153ec19fbf6e1257bac0c419a670f7
Juan Canelon <liscanojac@gmail.com>
2023-03-20 14:29:53 -0300

    feat: solving interfaces

--- projects/interfaces/playlist-soundness/src/index.ts
+++ projects/interfaces/playlist-soundness/src/index.ts
@@ -1,2 +1,74 @@
 // Write your unrollPlaylist function and types here! ✨
 // You'll need to export the function so the tests can run it.
+interface Song {
+	artist: string | Array<string>;
+	length: number;
+	name: string;
+	type: "song";
+}
+
+interface Album {
+	songs: Array<Song>;
+	type: "album";
+}
+
+interface Playlist {
+	resolve(): Array<Song>;
+	type: "playlist";
+}
+
+// type MusicInput = Array<Song> | Array<Album> | Array<Playlist>;
+type MusicInput = Array<Song | Album | Playlist>;
+
+interface PlaylistToReturn {
+	artists: {
+		[i: string]: Array<string>;
+	};
+	songs: Array<string>;
+	time: number;
+}
+
+export function unrollPlaylist(music: MusicInput): PlaylistToReturn {
+	const playlistToReturn: PlaylistToReturn = {
+		artists: {},
+		songs: [],
+		time: 0,
+	};
+
+	function addSong(song: Song) {
+		if (Array.isArray(song.artist)) {
+			for (let i = 0; i < song.artist.length; i++) {
+				let currentArtist = song.artist[i];
+				addArtist(currentArtist, song.name);
+			}
+		} else {
+			addArtist(song.artist, song.name);
+		}
+		playlistToReturn.songs.push(song.name);
+		playlistToReturn.time += song.length;
+	}
+
+	function addArtist(artist: string, song: string) {
+		if (!playlistToReturn.artists.hasOwnProperty(artist)) {
+			playlistToReturn.artists[artist] = [song];
+		} else {
+			playlistToReturn.artists[artist].push(song);
+		}
+	}
+
+	for (let i = 0; i < music.length; i++) {
+		let item: Song | Album | Playlist = music[i];
+		switch (item.type) {
+			case "song":
+				addSong(item);
+				break;
+			case "album":
+				item.songs.forEach((song: Song) => addSong(song));
+				break;
+			case "playlist":
+				item.resolve().forEach((song: Song) => addSong(song));
+				break;
+		}
+	}
+	return playlistToReturn;
+}
--- projects/interfaces/vacation-planning/03-landmarks/index.ts
+++ projects/interfaces/vacation-planning/03-landmarks/index.ts
@@ -1,2 +1,77 @@
 // Write your groupLandmarks function here! ✨
 // You'll need to export it so the tests can run it.
+
+interface BaseLandmark {
+	name: string;
+	type: string;
+}
+
+interface Mountain extends BaseLandmark {
+	type: "mountain";
+	height: number;
+}
+
+interface Park extends BaseLandmark {
+	type: "park";
+	acres: number;
+}
+
+interface Lighthouse extends BaseLandmark {
+	type: "lighthouse";
+	height: number;
+	lit: number;
+}
+
+interface Lake extends BaseLandmark {
+	type: "lake";
+	miles: number;
+}
+
+interface Waterfall extends BaseLandmark {
+	type: "waterfall";
+	height: number;
+}
+
+interface River extends BaseLandmark {
+	type: "river";
+	depth: number;
+	length: number;
+}
+
+interface Fort extends BaseLandmark {
+	type: "fort";
+}
+
+type Landmark = Mountain | Park | Lighthouse | Lake | Waterfall | River | Fort;
+
+export function describeLandmark(landmark: Landmark): string {
+	const message: Array<string> = [
+		`${landmark.name} is a ${landmark.type} in Upstate New York.`,
+	];
+
+	switch (landmark.type) {
+		case "lake":
+			message.push(`It covers ${landmark.miles} square miles of water.`);
+			break;
+		case "lighthouse":
+			message.push(
+				`It was first lit in ${landmark.lit} and is ${landmark.height} feet high.`
+			);
+			break;
+		case "mountain":
+			message.push(`Its peak is ${landmark.height} feet high.`);
+			break;
+		case "park":
+			message.push(`It covers ${landmark.acres} square acres.`);
+			break;
+		case "river":
+			message.push(
+				`It flows for ${landmark.length} miles and is ${landmark.depth} feet deep at its deepest.`
+			);
+			break;
+		case "waterfall":
+			message.push(`It is ${landmark.height} feet high.`);
+			break;
+	}
+	return message.join("\n");
+}

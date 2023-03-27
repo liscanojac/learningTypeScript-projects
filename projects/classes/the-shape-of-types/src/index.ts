859e56d0b2fbde70fcaa5c7a74539a0fffeb15a4
Juan Canelon <liscanojac@gmail.com>
2023-03-27 15:51:13 -0300

    feat: classes/theShapeOfTypes

--- projects/classes/the-shape-of-types/src/index.ts
+++ projects/classes/the-shape-of-types/src/index.ts
@@ -1,20 +1,73 @@
 // Write your classes here! ✨
 // You'll need to export them so the tests can run them.
 
-export abstract class Horror {
-	protected abstract readonly name: string;
-	protected evil: boolean;
-	abstract power: number;
-
-	doBattle(opponent: Horror) {}
-
-	getPower() {}
-
-	protected abstract getPowerFrom(opponent: Horror): number;
-
-	protected abstract isEvil(): boolean;
+interface Consumed {
+	evil: boolean;
+	name: string;
+	power: number;
 }
 
-class Demon extends Horror {
-	protected name: string = "Demon";
+export abstract class Horror {
+	abstract readonly name: string;
+
+	#consumed: Array<Consumed> = [];
+	#power: number = 0;
+
+	abstract isEvil(): boolean;
+	abstract getPowerFrom(previouslyConsumed: Consumed): number;
+
+	getPower(): number {
+		return this.#power;
+	}
+
+	doBattle(opponent: Horror) {
+		if (this.getPower > opponent.getPower) {
+			this.#consumes(opponent);
+		}
+	}
+
+	#consumes(opponent: Horror) {
+		this.#consumed.push({
+			name: opponent.name,
+			power: opponent.getPower(),
+			evil: opponent.isEvil(),
+		});
+		this.#power += opponent.getPower();
+	}
+}
+
+export class Demon extends Horror {
+	readonly name: string = "Demon";
+	#evil: boolean = true;
+
+	isEvil(): boolean {
+		return this.#evil;
+	}
+
+	getPowerFrom(previouslyConsumed: Consumed): number {
+		return previouslyConsumed.evil
+			? previouslyConsumed.power / 2
+			: previouslyConsumed.power * 2;
+	}
+}
+
+export class Sorcerer extends Horror {
+	readonly name: string;
+	#evil: boolean;
+
+	constructor(name: string, evil: boolean) {
+		super();
+		this.name = name;
+		this.#evil = evil;
+	}
+
+	isEvil(): boolean {
+		return this.#evil;
+	}
+
+	getPowerFrom(previouslyConsumed: Consumed): number {
+		return previouslyConsumed.evil === this.#evil
+			? previouslyConsumed.power * 2
+			: previouslyConsumed.power;
+	}
 }
